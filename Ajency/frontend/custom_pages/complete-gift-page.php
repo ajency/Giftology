5bf225c5ad6e4fd0e673719560477e1ee9704c2e
Ajaj Rajguru <ajaj@ajency.in>
2017-04-03 14:59:03 +0530

    action moved inside container

--- Ajency/frontend/custom_pages/complete-gift-page.php
+++ Ajency/frontend/custom_pages/complete-gift-page.php
@@ -224,14 +224,16 @@ if(is_user_logged_in()){
                             </div>
 
 
-                        </form>
-
-                        <div class="send-actions">
+                            <div class="send-actions">
                                 <!-- <button type="button" class="btn btn-default cancel">Cancel</button>-->
                                 <!-- <div class="group"> -->
                                 <button type="submit" id="update-gift" class="btn btn-default site-btn-2 save-data" disabled>Next</button>
                                 <!-- </div> -->
                             </div>
+
+
+                        </form>
+
 
 
 
